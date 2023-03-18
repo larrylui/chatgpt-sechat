0a5a90f94dc451d7a04bdb057f5777480193e665
larrylui <124667005+larrylui@users.noreply.github.com>
2023-03-18 23:50:05 +0800

    Update Generator.tsx

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -228,7 +228,7 @@ export default () => {
             class="gen-textarea"
           />
           <button onClick={handleButtonClick} disabled={systemRoleEditing()} gen-slate-btn>
-            發送
+            Send
           </button>
           <button title="清除" onClick={clear} disabled={systemRoleEditing()} gen-slate-btn>
             <IconClear />
