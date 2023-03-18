d4e0f1b6917a7fe01065daa62f7ffbf2ebe687d7
larrylui <124667005+larrylui@users.noreply.github.com>
2023-03-18 23:21:00 +0800

    Update Generator.tsx

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -207,7 +207,7 @@ export default () => {
         when={!loading()}
         fallback={() => (
           <div class="gen-cb-wrapper">
-            <span>AI is thinking...</span>
+            <span>Secret Chat正在思考...</span>
             <div class="gen-cb-stop" onClick={stopStreamFetch}>Stop</div>
           </div>
         )}
